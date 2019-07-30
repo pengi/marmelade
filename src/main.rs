d4f8963d6ca422b1caf5605c640625b6a67a2651
Max Sikstrom <max@pengi.se>
2019-07-30 19:26:24 +0200

    Specify image from command line

--- src/main.rs
+++ src/main.rs
@@ -1,16 +1,28 @@
+#[macro_use]
+extern crate clap;
+
 use marmelade::filesys::hfs;
 use marmelade::filesys::hfs::FileAdaptor;
 use std::fs;
 
 fn main() {
-    let file = fs::File::open("ref/refdisk.dmg").unwrap();
-    let fa = FileAdaptor::new(file);
+    let matches = clap_app!(myapp =>
+        (version: "0.1")
+        (author: "Max Sikström <max@pengi.se>")
+        (about: "Makes toasters fly - run old stuff on new machines")
+        (@arg img: +required -i --image +takes_value "Image file")
+        (@arg file: -f --file +takes_value "File to read")
+    ).get_matches();
+
+    let img = fs::File::open(matches.value_of("img").unwrap()).unwrap();
+    let fa = FileAdaptor::new(img);
     let fs = hfs::HfsImage::from(fa).unwrap();
 
-    println!("{:#?}", fs);
-
-    fs.list_recursive(1, 0);
-    let file = fs.catalog.locate("SimpleText");
-    println!("File: {:#?}", file);
-    // fs.list_files().unwrap();
+    if let Some(file) = matches.value_of("file") {
+        let file = fs.catalog.locate(file);
+        println!("File: {:#?}", file);
+    } else {
+        // println!("Image: {:#?}", fs);
+        fs.list_recursive(1, 0);
+    }
 }
