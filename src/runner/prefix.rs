697dbe14c0806cb40e93a1d3520a0aac06219622
Max Sikstrom <max@pengi.se>
2019-08-08 12:37:52 +0200

    Add simple prefix map

--- src/runner/prefix.rs
+++ src/runner/prefix.rs
@@ -39,10 +39,45 @@ impl Prefix {
     }
 }
 
+pub struct PrefixMap<T> {
+    // Keep a simple implementation for now
+    children: Vec<(Prefix, T)>
+}
+
+impl<T> From<Vec<(Prefix, T)>> for PrefixMap<T> {
+    fn from(vec: Vec<(Prefix, T)>) -> PrefixMap<T> {
+        PrefixMap {
+            children: vec
+        }
+    }
+}
+
+impl<T> PrefixMap<T> {
+    fn locate(&self, address : u32) -> Option<&T> {
+        for (prefix, ref value) in self.children.iter() {
+            if prefix.contains_value(address) {
+                return Some(value);
+            }
+        }
+        None
+    }
+    fn locate_mut(&mut self, address : u32) -> Option<&mut T> {
+        for (prefix, ref mut value) in self.children.iter_mut() {
+            if prefix.contains_value(address) {
+                return Some(value);
+            }
+        }
+        None
+    }
+}
+
 
 #[cfg(test)]
 mod tests {
-    use super::Prefix;
+    use super::{
+        Prefix,
+        PrefixMap
+    };
 
     #[test]
     fn address_prefix_mask() {
@@ -104,5 +139,39 @@ mod tests {
         assert!(!Prefix::new(0x43000000, 9).contains_prefix(Prefix::new(0x43800000,32)));
 
         assert!(!Prefix::new(0x12340000, 24).contains_prefix(Prefix::new(0x12340000,16)));
+    }
+
+    #[test]
+    fn locate_map() {
+        let map = PrefixMap::from(vec![
+            (Prefix::new(0x00000000, 20), 1),
+            (Prefix::new(0x00001000, 20), 2),
+            (Prefix::new(0x20000000, 8), 3),
+            (Prefix::new(0x40000000, 8), 4)
+        ]);
+        assert_eq!(map.locate(0x00000123), Some(&1));
+        assert_eq!(map.locate(0x00001000), Some(&2));
+        assert_eq!(map.locate(0x00001123), Some(&2));
+        assert_eq!(map.locate(0x00003123), None);
+        assert_eq!(map.locate(0x10003123), None);
+        assert_eq!(map.locate(0x20003123), Some(&3));
+        assert_eq!(map.locate(0x30003123), None);
+    }
+
+    #[test]
+    fn locate_mut_map() {
+        let mut map = PrefixMap::from(vec![
+            (Prefix::new(0x00000000, 20), 1),
+            (Prefix::new(0x00001000, 20), 2),
+            (Prefix::new(0x20000000, 8), 3),
+            (Prefix::new(0x40000000, 8), 4)
+        ]);
+        assert_eq!(map.locate_mut(0x00000123), Some(&mut 1));
+        assert_eq!(map.locate_mut(0x00001000), Some(&mut 2));
+        assert_eq!(map.locate_mut(0x00001123), Some(&mut 2));
+        assert_eq!(map.locate_mut(0x00003123), None);
+        assert_eq!(map.locate_mut(0x10003123), None);
+        assert_eq!(map.locate_mut(0x20003123), Some(&mut 3));
+        assert_eq!(map.locate_mut(0x30003123), None);
     }
 }
